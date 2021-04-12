4133419ff02fdc1066455839da8c5106943ea300
Jorge Rojas <jl.rojas@viaducto.io>
2021-04-12 17:56:18 -0500

    add cypress
    Co-authored-by: Juan Olvera <jp-olvera@users.noreply.github.com>

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -23,3 +24,27 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (overrides = {}) => {
+  Cypress.log({
+    name: 'loginViaAuth0',
+  });
+
+  const options = {
+    method: 'POST',
+    url: Cypress.env('auth_url'),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: {
+      grant_type: 'password',
+      username: Cypress.env('auth_username'),
+      password: Cypress.env('auth_password'),
+      audience: Cypress.env('auth_audience'),
+      scope: 'openid profile email',
+      client_id: Cypress.env('auth_client_id'),
+      client_secret: Cypress.env('auth_client_secret'),
+    },
+  };
+  cy.request(options);
+});
