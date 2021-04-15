c52acc66e3278b4f2efdcdf857a3e7c8f9290812
jp-olvera <jp.olvera@viaducto.io>
2021-04-15 14:32:48 -0500

    ci: configure webpack build

--- src/components/Buttons/LoginButton.js
+++ src/components/Buttons/LoginButton.js
@@ -1,14 +1,21 @@
-import React from "react";
-import { Button } from '@jp-olvera/jp-viaducto-components'
+import React from 'react';
+import { Button } from '@jp-olvera/jp-viaducto-components';
 import { FormattedMessage } from 'react-intl';
 
 const LoginButton = ({ callback }) => {
   let colors = {
-    default: "transparent",
-    hover: "transparent",
-    click: "transparent"
-  }
-  return <Button onClick={callback} colors={colors} data-cy="button-login" label={<FormattedMessage id="buttonLogin" />} />;
-}
+    default: 'transparent',
+    hover: 'black',
+    click: 'transparent',
+  };
+  return (
+    <Button
+      onClick={callback}
+      colors={colors}
+      data-cy="button-login"
+      label={<FormattedMessage id="buttonLogin" />}
+    />
+  );
+};
 
 export default LoginButton;
