40984a34d53be27a18097821b8ea7d53bcee6377
jp-olvera <jp.olvera@viaducto.io>
2021-04-15 16:06:01 -0500

    feat: remove render and export component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
-import { I18Provider } from './i18n'
+import { I18Provider } from './i18n';
 import Login from './components/Login';
 
 //TODO: Accesibilidad vamos bien
@@ -9,18 +9,35 @@ import Login from './components/Login';
 //TODO: hacer las redirecciones a los subdominios
 //TODO: montar en webflow
 
-ReactDOM.render(
-  <Auth0Provider
-    clientId={process.env.REACT_APP_CLIENT_ID}
-    domain={process.env.REACT_APP_DOMAIN}
-    redirectUri={window.location.origin}
-    audience={"https://auth0-back"}
-  >
-    <React.StrictMode>
+// ReactDOM.render(
+//   <Auth0Provider
+//     clientId={process.env.REACT_APP_CLIENT_ID}
+//     domain={process.env.REACT_APP_DOMAIN}
+//     redirectUri={window.location.origin}
+//     audience={"https://auth0-back"}
+//   >
+//     <React.StrictMode>
+//       <I18Provider>
+//         <Login />
+//       </I18Provider>
+//     </React.StrictMode>
+//   </Auth0Provider>,
+//   document.getElementById('root')
+// );
+
+const LoginComponent = () => {
+  return (
+    <Auth0Provider
+      clientId={process.env.REACT_APP_CLIENT_ID}
+      domain={process.env.REACT_APP_DOMAIN}
+      redirectUri={window.location.origin}
+      audience={'https://auth0-back'}
+    >
       <I18Provider>
         <Login />
       </I18Provider>
-    </React.StrictMode>
-  </Auth0Provider>,
-  document.getElementById('root')
-);
+    </Auth0Provider>
+  );
+};
+
+export default LoginComponent;
